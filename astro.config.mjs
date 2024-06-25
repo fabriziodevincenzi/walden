e41f72d1c3fc7f506a2896c4554f510dbf425d71
Fabrizio De Vincenzi <hello@fabriziodevincenzi.com>
2024-06-25 17:53:28 +0200

    Optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 
 export default defineConfig({
     site: 'https://www.vitaneiboschi.it',
+    integrations: [image()],
   })
